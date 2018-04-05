2b625193ced4e9606f337bd6846bc1a6b60fd565
Emmanuel Iloh <emmanueli.work@gmail.com>
2018-04-05 22:40:20 +0800

    Scores
    added a scoring system for the user and bot

--- Main.py
+++ Main.py
@@ -5,21 +5,27 @@ os.system('cls||clear')
 random.seed()
 
 options = ['p', 's', 'r']
+scores = [0, 0]
 
 while True:
     botMove = options[random.randrange(0,3)]
     userMove = input("\n\np - paper \ns - scissors\nr - rock\nq - quit\n\nSelect: ")
     os.system('cls||clear')
 
+
     if userMove == botMove :
-        print("it's a draw")
-        print(userMove, botMove)
+        print(" It's a Draw")
+        print("", userMove, "and", botMove, "are the same")
 
     elif (userMove == 'p' and botMove == 'r') or (userMove == 'r' and botMove == 's') or (userMove == 's' and botMove == 'p') :
         print(" You Win!!\n", userMove +" beats ", botMove)
+        scores[0] += 1
 
     elif (userMove == 'q') :
         break
 
     else :
         print (" You Lose!!\n", botMove +" beats ", userMove)
+        scores[1] += 1
+
+    print(" Scores: user ", scores[0], "bot ", scores[1])
